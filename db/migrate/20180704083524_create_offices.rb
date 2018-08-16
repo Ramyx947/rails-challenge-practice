737f1b747df559eabd50b3efefbe65b85cf9c7a0
github username <github email address>
2018-08-16 13:19:41 +0100

    first draft

--- db/migrate/20180704083524_create_offices.rb
+++ db/migrate/20180704083524_create_offices.rb
@@ -1,6 +1,9 @@
 class CreateOffices < ActiveRecord::Migration[5.1]
   def change
     create_table :offices do |t|
+      t.belongs_to :building, index: true
+      t.belongs_to :company, index: true
+      t. integer :floor
 
       t.timestamps
     end
